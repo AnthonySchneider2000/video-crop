e8359d0d071a24b8ada78cf7508073b16aa1e1f5
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-14 01:40:17 -0500

    added skeletons for loading time

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,6 +5,15 @@ import { useState, useRef, useEffect } from "react";
 import { useFileContext } from "@/lib/FileContext";
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { fetchFile, toBlobURL } from "@ffmpeg/util";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const Header = () => {
+  return (
+    <header className="flex h-40 flex-row content-center items-center justify-center">
+      <h1 className="text-4xl font-bold">Video Cropper</h1>
+    </header>
+  );
+};
 
 function HomePage() {
   const { selectedFiles } = useFileContext();
@@ -15,7 +24,13 @@ function HomePage() {
   const ffmpegRef = useRef(new FFmpeg());
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const messageRef = useRef<HTMLParagraphElement | null>(null);
-  
+
+  const downloadFromYouTube = async () => {
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const load = async () => {
     const baseURL = "https://unpkg.com/@ffmpeg/core-mt@0.12.4/dist/esm";
@@ -73,59 +88,74 @@ function HomePage() {
   };
 
   useEffect(() => {
-
-    if(crossOriginIsolated) {
+    if (crossOriginIsolated) {
       console.log("Cross Origin Isolated");
     }
     load();
   }, []);
 
   return loaded ? (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-4 p-4">
-      <h1 className="text-4xl font-bold">Video Cropper</h1>
-      {selectedFiles.length > 0 && (
-        <div className="flex flex-row justify-center content-center gap-4">
-          <video
-            controls
-            className="h-80 w-80"
-            src={URL.createObjectURL(selectedFiles[0])}
-          ></video>
-          <video controls className="h-80 w-80" ref={videoRef}></video>
+    <>
+      <Header />
+      <div className="flex h-full w-full flex-col items-center justify-center gap-4 p-4">
+        {selectedFiles.length > 0 && (
+          <div className="flex flex-row content-center justify-center gap-4">
+            <video
+              controls
+              className="h-80 w-80"
+              src={URL.createObjectURL(selectedFiles[0])}
+            ></video>
+            <video controls className="h-80 w-80" ref={videoRef}></video>
+          </div>
+        )}
+        <DropzoneComponent size="long" />
+        <Input
+          type="text"
+          placeholder="YouTube URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <div className="flex w-full flex-row content-evenly justify-evenly gap-4 ">
+          <Input
+            type="number"
+            className=""
+            placeholder="Start Time"
+            onChange={(e) => setStartTime(parseInt(e.target.value))}
+          />
+          <Input
+            type="number"
+            className=""
+            placeholder="End Time"
+            onChange={(e) => setEndTime(parseInt(e.target.value))}
+          />
         </div>
-      )}
-      <DropzoneComponent size="long" />
-      <Input
-        type="text"
-        placeholder="YouTube URL"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-      />
-      <div className="flex w-full flex-row content-evenly justify-evenly gap-4 ">
-        <Input
-          type="number"
-          className=""
-          placeholder="Start Time"
-          onChange={(e) => setStartTime(parseInt(e.target.value))}
-        />
-        <Input
-          type="number"
-          className=""
-          placeholder="End Time"
-          onChange={(e) => setEndTime(parseInt(e.target.value))}
-        />
+        <div className="flex w-full flex-row content-evenly justify-evenly gap-4">
+          <Button onClick={downloadFromYouTube} className="p-2">
+            Download from YouTube
+          </Button>
+          <Button onClick={transcode} className="p-2">
+            Crop Video
+          </Button>
+        </div>
       </div>
-      <div className="flex w-full flex-row content-evenly justify-evenly gap-4">
-        <Button className="p-2">Download from YouTube</Button>
-        <Button onClick={transcode} className="p-2">
-          Crop Video
-        </Button>
-      </div>
-    </div>
+    </>
   ) : (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-4 p-4">
-      <h1 className="text-4xl font-bold">Video Cropper</h1>
-      <p ref={messageRef}></p>
-    </div>
+    <>
+      <Header />
+      <div className="flex h-full w-full flex-col items-center justify-center gap-4 p-4">
+        <Skeleton className="h-20 w-full rounded-md" />
+        <Skeleton className="h-10 w-full rounded-md" />
+        <div className="flex w-full flex-row content-evenly justify-evenly gap-4 ">
+          <Skeleton className="h-10 w-1/2 rounded-md" />
+          <Skeleton className="h-10 w-1/2 rounded-md" />
+          </div>
+        <div className="flex w-full flex-row content-evenly justify-evenly gap-4">
+          <Skeleton className="h-20 w-1/2 rounded-md" />
+          <Skeleton className="h-20 w-1/2 rounded-md" />
+        </div>
+
+      </div>
+    </>
   );
 }
 
