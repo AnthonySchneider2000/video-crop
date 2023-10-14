5234efa8c0357ac012214ee1f8029e192a517101
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-14 00:10:27 -0500

    works properly

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import DropzoneComponent from "@/components/ui/dropzone";
 import { useState, useRef, useEffect } from "react";
 import { useFileContext } from "@/lib/FileContext";
 import { FFmpeg } from "@ffmpeg/ffmpeg";
-import { fetchFile, toBlobURL } from '@ffmpeg/util'
+import { fetchFile, toBlobURL } from "@ffmpeg/util";
 
 function HomePage() {
   const { selectedFiles } = useFileContext();
@@ -13,8 +13,8 @@ function HomePage() {
   const [url, setUrl] = useState("");
   const [loaded, setLoaded] = useState(false);
   const ffmpegRef = useRef(new FFmpeg());
-  const videoRef = useRef<HTMLVideoElement | null>(null)
-  const messageRef = useRef<HTMLParagraphElement | null>(null)
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const messageRef = useRef<HTMLParagraphElement | null>(null);
 
   const load = async () => {
     const baseURL = "https://unpkg.com/@ffmpeg/core-mt@0.12.4/dist/esm";
@@ -29,11 +29,11 @@ function HomePage() {
       coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
       wasmURL: await toBlobURL(
         `${baseURL}/ffmpeg-core.wasm`,
-        "application/wasm"
+        "application/wasm",
       ),
       workerURL: await toBlobURL(
         `${baseURL}/ffmpeg-core.worker.js`,
-        "text/javascript"
+        "text/javascript",
       ),
     });
     setLoaded(true);
@@ -41,16 +41,33 @@ function HomePage() {
   };
 
   const transcode = async () => {
-    const videoURL = "https://ffmpegwasm.netlify.app/video/video-15s.avi";
+    const videoURL = selectedFiles[0]
+      ? URL.createObjectURL(selectedFiles[0])
+      : null;
+    if (!videoURL) {
+      console.log("No video selected");
+      return;
+    }
     const ffmpeg = ffmpegRef.current;
     await ffmpeg.writeFile("input.avi", await fetchFile(videoURL));
-    await ffmpeg.exec(["-i", "input.avi", "output.mp4"]);
-    const fileData = await ffmpeg.readFile('output.mp4');
+    // trim video from startTime to endTime
+    await ffmpeg.exec([
+      "-i",
+      "input.avi",
+      "-ss",
+      `${startTime}`,
+      "-to",
+      `${endTime}`,
+      "-c",
+      "copy",
+      "output.mp4",
+    ]);
+    const fileData = await ffmpeg.readFile("output.mp4");
     const data = new Uint8Array(fileData as ArrayBuffer);
     if (videoRef.current) {
       videoRef.current.src = URL.createObjectURL(
-        new Blob([data.buffer], { type: 'video/mp4' })
-      )
+        new Blob([data.buffer], { type: "video/mp4" }),
+      );
     }
   };
 
@@ -61,14 +78,16 @@ function HomePage() {
   return loaded ? (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-4 p-4">
       <h1 className="text-4xl font-bold">Video Cropper</h1>
-      <div>
-        {/* selectedFiles */}
-        {selectedFiles.map((file) => (
-          <div key={file.name}>
-            {file.name} - {file.size} bytes
-            </div>
-        ))}
-      </div>
+      {selectedFiles.length > 0 && (
+        <div className="flex flex-row justify-center content-center gap-4">
+          <video
+            controls
+            className="h-80 w-80"
+            src={URL.createObjectURL(selectedFiles[0])}
+          ></video>
+          <video controls className="h-80 w-80" ref={videoRef}></video>
+        </div>
+      )}
       <DropzoneComponent size="long" />
       <Input
         type="text"
@@ -92,7 +111,9 @@ function HomePage() {
       </div>
       <div className="flex w-full flex-row content-evenly justify-evenly gap-4">
         <Button className="p-2">Download from YouTube</Button>
-        <Button onClick={transcode} className="p-2">Crop Video</Button>
+        <Button onClick={transcode} className="p-2">
+          Crop Video
+        </Button>
       </div>
     </div>
   ) : (
