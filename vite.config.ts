f4bfe58d6cf10cf2c162531df8ea5f6b78db1101
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-14 00:15:45 -0500

    testing if this fixes deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
 
 export default defineConfig({
-  plugins: [react(), crossOriginIsolation()],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
